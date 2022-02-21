e22a67adbb77acba9a873206d481068e5b750bb3
giorgio1966 <jhaycald@hotmail.com>
2022-02-21 08:59:08 -0500

    sexta entrega
    sexta entrega---faltaba la sintesis

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -3,18 +3,12 @@
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
-    p Con el diagrama de actividades se termina la descripción de las tres técnicas usadas para la especificación y el análisis de requisitos, como se pudo analizar cada una de las técnicas nos brinda características para lograr representar gráficamente y de una forma clara, los requisitos que un usuario necesita poder cumplir satisfactoriamente con las solicitudes de los clientes y usuarios, por otro lado se pueden usar todas las técnicas en una especificación y análisis de requisitos si se ve la necesidad.
-    p.mb-5 A continuación, se muestra un mapa conceptual con los elementos más importantes desarrollados en este componente.
-
-    .row.justify-content-center
-      .col-lg-10.mb-5
-        figure
-          img(src="@/assets/curso/sintesis.svg", alt="alt")
-      .col-auto
-        a.anexo.mb-4(:href="obtenerLink('/downloads/prueba.pdf')" target="_blank")
-          .anexo__icono
-            img(src="@/assets/template/icono-pdf.svg")
-          .anexo__texto
+    img(src="@/assets/curso/sintesis/sintesis.jpg", alt="Síntesis")
+    .row.bg-gris.me-5.ms-5.mb-5.p-3.Neg-Top40.position-relative.rounded
+      .col-12
+        p.mb-0 Los frameworks como Hibernate facilitan la conexión y manipulación de bases de datos relacionales ya que vienen configurados y optimizados para abstraer la complejidad propia de las bases de datos y permiten al desarrollador enfocarse en trabajar únicamente con el paradigma orientado a objetos. Adicionalmente una vez configurado el Framework facilita la realización de todo tipo de operación DML (SELECT, INSERT, UPDATE y DELETE) acelerando la velocidad en la que se pueden desarrollar aplicaciones JAVA que gestionan bases de datos relacionales. A continuación, se muestra un mapa conceptual de los elementos más importantes desarrollados en el primer apartado de este componente:
+    img(src="@/assets/curso/sintesis/diagrama.svg", alt="Síntesis")
+          //-.anexo__texto
             p Anexo. Síntesis
 
 </template>
